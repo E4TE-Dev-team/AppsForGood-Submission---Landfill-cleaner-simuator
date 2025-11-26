7bf8feb33274feb1cce1f865f42c332a01ce47a9
Willkoala66 <willkoala6@outlook.com>
2025-11-26 16:37:15 +0000

    starting

---
+++ test.py
@@ -0,0 +1,28 @@
+__docformat__ = "reStructuredText"
+import sys
+import os
+import subprocess
+
+import pygame
+
+import pymunk
+import pymunk.pygame_util
+from pymunk import Vec2d
+
+
+def main():
+
+
+
+    hello()
+
+    pygame.init()
+    screen = pygame.display.set_mode((600, 600))
+    clock = pygame.time.Clock()
+    running = True
+
+    ### Physics stuff
+    space = pymunk.Space()
+    space.gravity = Vec2d(0.0, -900.0)
+
+
