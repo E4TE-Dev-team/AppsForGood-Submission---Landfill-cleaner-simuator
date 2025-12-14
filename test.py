6f2dd747d48aedaca1e5c410c36a25c35159f5c7
Willkoala66 <willkoala6@outlook.com>
2025-12-14 12:06:54 +0000

    look at drawin game

--- test.py
+++ test.py
@@ -10,6 +10,25 @@ import pymunk.pygame_util
 from pymunk import Vec2d
 
 
+
+def getinput():
+    return 0
+
+def update(space):
+    return 0
+
+def draw(space):
+    shapeinworldgeometry = 5
+    #tmp var
+
+    if shapeinworldgeometry.pos() > playerxpos+50 and shapeinworldgeometry.pos() > playerypos+50:
+        #skip because it is too far away
+        pass
+
+
+    return 0
+
+
 def main():
 
     pygame.init()
@@ -22,8 +41,17 @@ def main():
     space.gravity = Vec2d(0.0, -900.0)
     
     zoo_level = [
-    [{"x" : 0},{"y" : 0},{"z" : 0},{"r" : 0},{"type" : "block"},{"texture" : "test"}],
-
-
+    {"x" : 0 ,"y" : 0 ,"r" : 0,"type" : "block","texture" : "test"},
+    {"x" : -1,"y" : 0,"r" : 180,"type" : "slope","texture" : "test"}
     ]
-    
+    #level geometry pymunk.Body
+    world = pymunk.Body()
+    world.__init__(0,0,STATIC)
+    space.add(world)
+
+
+
+    while running == True:
+        getinput()
+        update(space)
+        draw(space)
