dfef558971adf7b455712838028b95338b633f5c
e4te-devs <willkoala6@outlook.com>
2026-01-23 12:47:17 +0000

    player rotaltion

--- ai.py
+++ ai.py
@@ -17,6 +17,8 @@ if args.debug:
 # Configuration
 WIDTH, HEIGHT = 800, 600
 FPS = 120
+bottle_collect_dist = 20
+playerdirection = False
 
 def getimg(name):
     return pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)), name))
@@ -30,6 +32,8 @@ zoo_level = [
     {"x": 428, "y": 350, "w": 75,  "h": 50, "type": "block"}, # platforming block
 ]
 bottle_img = getimg("bottle.png")
+player_img = getimg("player.png")
+player_reversed_img = getimg("player_reversed.png")
 
 
 def create_structure(space, info):
@@ -45,40 +49,48 @@ def create_structure(space, info):
 
 def create_player(space, pos):
     """Creates a physics-enabled player circle."""
+    player_shape = [(11, 22), (-11, 22), (-11, -13), (11, -13)]
     mass = 1
-    moment = pymunk.moment_for_poly(mass, [(-10,0), (0, -10), (10, 0), (0,10)])
+    moment = pymunk.moment_for_poly(mass, player_shape)
     body = pymunk.Body(mass, moment)
     body.position = pos
-    shape = pymunk.Poly(body, [(-10,0), (0, -10), (10, 0), (0,10)])
+    body.angle = 0
+    shape = pymunk.Poly(body, player_shape)
     shape.friction = 0.8
     space.add(body, shape)
     return shape
 
-def getinput(player_shape):
+def getinput(player_shape, isreversed):
     """Handles movement: Left, Right, and Jump."""
     keys = pygame.key.get_pressed()
     v = player_shape.body.velocity
-
+    player_shape.body.angle = 0
+    
     if keys[pygame.K_LEFT]:
         player_shape.body.velocity = (-200, v.y)
+        isreversed = True
     elif keys[pygame.K_RIGHT]:
         player_shape.body.velocity = (200, v.y)
+        isreversed = False
     else:
         player_shape.body.velocity = (0, v.y)
 
     # Simple Jump (only if vertical velocity is near zero)
     if keys[pygame.K_UP] and abs(v.y) < 0.2:
         player_shape.body.velocity = (v.x, -400)
+        player_shape.body.angle = 0 
     elif keys[pygame.K_f]:
         print("VEL: " + str(player_shape.body.velocity) +  " POS: " + str(player_shape.body.position) + " ANGLE: " + str(player_shape.body.angle))
+    return isreversed
 
 def create_bottle(space, pos: Vec2d):
     """Creates a physics-enabled bottle."""
     mass = 1
-    moment = pymunk.moment_for_poly(mass, [(-10,0), (0, -10), (10, 0), (0,10)])
+    bottle_shape = [(-10,0), (0, -10), (10, 0), (0,10)]
+    moment = pymunk.moment_for_poly(mass, bottle_shape)
     body = pymunk.Body(mass, moment)
     body.position = pos
-    shape = pymunk.Poly(body, [(-10,0), (0, -10), (10, 0), (0,10)])
+    shape = pymunk.Poly(body, bottle_shape)
     shape.friction = 0.8
     space.add(body, shape)
     return shape
@@ -86,12 +98,12 @@ def create_bottle(space, pos: Vec2d):
 def create_bottles(space):
     """Creates bottles in the level."""
     bottles = []
-    bottle_x = [50, 60, 70, 80, 90, 100]
+    bottle_x = [50, 60, 70, 80, 90, 100, 110, 120]
     for i in range(len(bottle_x)):
         bottles.append(create_bottle(space, (bottle_x[i], 200)))
     return bottles
 
-def main():
+def main(playerdirection):
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Landfill Cleaner - Apps for Good")
@@ -118,7 +130,7 @@ def main():
                 running = False
 
         # 1. Input
-        getinput(player)
+        playerdirection = getinput(player, playerdirection)
 
         # if player out of bounds, reset
         if player.body.position.y > HEIGHT:
@@ -136,9 +148,13 @@ def main():
             print("Player angle: ", player.body.angle)
 
         # 2. Physics Update
-        dt = 0.01 / FPS
-        for i in range(100):
+        num = 150
+        phy = (1/num)
+        dt = phy / FPS
+        for i in range(num):
+            player.body.angle = 0
             space.step(dt)
+            player.body.angle = 0
         # 3. Drawing
         screen.fill((200, 230, 255)) # Sky Blue
 
@@ -156,13 +172,16 @@ def main():
         screen.blit(text, (5, y))
         screen.blit(bottle_text, (5, y+10))
 
-        screen.blit(bottle_img, player.body.position-Vec2d(17,20))
-        
+        if playerdirection:
+            screen.blit(player_reversed_img, player.body.position-Vec2d(17,10))
+        else:
+            screen.blit(player_img, player.body.position-Vec2d(17,10))
+
         for item in items:
             if item.body.position.y > HEIGHT:
                 space.remove(item.body, item)
                 items.remove(item)
-            elif item.body.position <= player.body.position+Vec2d(10, 0) and item.body.position >= player.body.position-Vec2d(10, 0):
+            elif item.body.position.x <= player.body.position.x+bottle_collect_dist and item.body.position.x >= player.body.position.x-bottle_collect_dist and item.body.position.y <= player.body.position.y+bottle_collect_dist and item.body.position.y >= player.body.position.y-bottle_collect_dist:
                 space.remove(item.body, item)
                 items.remove(item)
                 print("Bottle collected!")
@@ -171,7 +190,7 @@ def main():
                 screen.blit(bottle_img, item.body.position-Vec2d(17,20))
 
         # Draw Player (using pymunk helper for simplicity)
-        if debug == True:
+        if debug:
             space.debug_draw(draw_options)
 
         pygame.display.flip()
@@ -181,4 +200,5 @@ def main():
     pygame.quit()
 
 if __name__ == "__main__":
-    main()
+    main(playerdirection)
+
